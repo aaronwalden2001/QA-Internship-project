0e25f77d623d1df5828754b3ccf089142767594c
Aaron Walden <walden.aaron01@outlook.com>
2026-02-24 15:28:57 -0500

    QAAR-1289:mobile emulation w/updates for other tasks

--- features/environment.py
+++ features/environment.py
@@ -1,18 +1,26 @@
 import os
+
+from requests import session
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-def browser_init(context, scenario):
+def browser_init(context, scenario_name):
+
+    ### MOBILE EMULATION ###
+    mobile_emulation = {"deviceName": "Nexus 5"}
+    chrome_options = ChromeOptions()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+
     driver_path = ChromeDriverManager().install()
     service = ChromeService(driver_path)
     context.driver = webdriver.Chrome(service=service)
-    context.driver = webdriver.Chrome()
     ### BROWSER OPTIONS ###
     #context.driver = webdriver.Firefox()
     # context.driver = webdriver.Chrome()
@@ -24,8 +32,8 @@ def browser_init(context, scenario):
     #     options=options
     # )
     ### BROWSERSTACK ###
-    bs_user = '******'
-    bs_key = '*****'
+    bs_user = 'aaronwalden_mgQwhi'
+    bs_key = '1Y9kdSFyKkyqgyBUhNQi'
     url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
     options = ChromeOptions()
     bstack_options = {
@@ -33,11 +41,28 @@ def browser_init(context, scenario):
     "osVersion" : "11",
     "browserVersion" : "latest",
     'browserName': 'Chrome',
-    'sessionName': scenario,
+    'sessionName': scenario_name,
     }
     options.set_capability('bstack:options', bstack_options)
     context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, 10)
 
+def before_scenario(context, scenario):
+    print('\nStarted scenario:', scenario.name)
+    browser_init(context, scenario.name)
+
+
+def before_step(context, step):
+    print('\nStarted step:', step.name)
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        print('\nStep failed:', step.name)
+
+
+def after_scenario(context, scenario):
+    context.driver.quit()
